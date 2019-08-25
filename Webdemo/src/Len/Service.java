eac74496bafe850b8f5bf1d3e3e88b1feac8712b
yohren00 <yohren00@gmil.com>
2019-08-25 21:06:00 +0800

    JDBC連結資料庫:新增、顯示全資料庫資料

---
+++ Webdemo/src/Len/Service.java
@@ -0,0 +1,27 @@
+package Len;
+
+import java.sql.Date;
+import java.util.List;
+
+import Len.dao.EmpDAO;
+
+public class Service {
+	
+	public void addEmp(String lastName , String firstName ,String birthDate) {
+		birthDate =birthDate.replace("/", "-");
+		Date date = Date.valueOf(birthDate);
+		
+		EmpDAO empdao = new EmpDAO();
+		empdao.insert(lastName, firstName, date);
+		
+	}
+	
+	public List<String[]> findAllEmp(){
+		EmpDAO empdao = new EmpDAO();
+		List<String[]> list = empdao.selectAll();
+		
+		
+		return list;
+	}
+
+}
